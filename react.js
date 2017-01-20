f64eac47293d74a9d1efabe9cae404f9b3bd8ed7
Hassan Khan <hassan.khan@endemolshine.com>
2017-01-20 03:41:15 +0000

    Initial commit

---
+++ react.js
@@ -0,0 +1,56 @@
+module.exports = {
+  'parser' : 'babel-eslint',
+
+  'extends' : ['eslint:recommended', 'plugin:react/recommended'],
+
+  'parserOptions' : {
+    'ecmaFeatures' : {
+      'jsx'           : true,
+    },
+  },
+
+  'plugins' : [
+    'import',
+    'react',
+  ],
+
+  'rules' : {
+
+    /**
+     * Imports
+     */
+
+     'import/extensions'          : [1, { "js": "never", "jsx": "never" }],
+
+    /**
+     * React
+     */
+    'react/display-name'                 : 1,
+    'react/forbid-prop-types'            : [2, { 'forbid' : ['any'] }],
+    'react/jsx-boolean-value'            : 1,
+    'react/jsx-closing-bracket-location' : [1, 'tag-aligned'],
+    'react/jsx-curly-spacing'            : 1,
+    'react/jsx-indent'                   : [1, 2],
+    'react/jsx-indent-props'             : [1, 2],
+    'react/jsx-key'                      : 1,
+    'react/jsx-max-props-per-line'       : 0,
+    'react/jsx-no-bind'                  : [1, { 'allowArrowFunctions': true }],
+    'react/jsx-no-literals'              : 0,
+    'react/jsx-pascal-case'              : 1,
+    'react/jsx-space-before-closing'     : 1,
+    'react/sort-prop-types'              : [1, {
+      "callbacksLast": true,
+      "requiredFirst": true,
+    }],
+    'react/jsx-sort-props'               : [1, {
+      "callbacksLast": true,
+      "shorthandFirst": true,
+    }],
+    'react/jsx-wrap-multilines'          : 2,
+    'react/no-multi-comp'                : 1,
+    'react/no-set-state'                 : 0,
+    'react/prefer-es6-class'             : 1,
+    'react/self-closing-comp'            : 1,
+    'react/sort-comp'                    : 1,
+  },
+};
